f5e5eb474e1a975cf8d6d3b84af0a299ff3de8dc
PlopesK <popi081004@hotmail.com>
2023-08-08 22:27:12 -0300

    While

--- HTML-CSS-PHP-JS/JS/Basic Exercises/loop.js
+++ HTML-CSS-PHP-JS/JS/Basic Exercises/loop.js
@@ -41,3 +41,42 @@ for (let i = 0; i < myArr.length; i++) {
   console.log("Value " + [i + 1] + ": " + myArr[i]);
 }
 console.log("Total: " + total);
+
+/* ------------- */
+
+function multiplyAll(arr) {
+  let product = 1;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      console.log("Number: " + arr[i][j]);
+      product = product * arr[i][j];
+      console.log("Product: " + product + "\n");
+    }
+  }
+  console.log("--------------------\n");
+  return product;
+}
+
+multiplyAll([
+  [1, 2],
+  [3, 4],
+  [5, 6, 7],
+]);
+multiplyAll([[1], [2], [3]]);
+multiplyAll([
+  [5, 1],
+  [0.2, 4, 0.5],
+  [3, 9],
+]);
+
+/* ----------------------------------- */
+//WHILE
+const myArrWhile = [];
+let cont = 10;
+
+do {
+  myArrWhile.push(i);
+  cont++;
+  console.log(myArrWhile);
+} while (i < 10);
+console.log(cont);
