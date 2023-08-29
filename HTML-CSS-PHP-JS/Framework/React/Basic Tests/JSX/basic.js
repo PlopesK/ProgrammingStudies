22870669f804e84e41cf32be7f2baee5e34b6807
PlopesK <popi081004@hotmail.com>
2023-08-28 21:19:23 -0300

    Change app1 name and seeing basic JSX exercises

---
+++ HTML-CSS-PHP-JS/Framework/React/Basic Tests/JSX/basic.js
@@ -0,0 +1,13 @@
+const JSX = (
+  <div>
+    <h1> Hello JSX!</h1>
+    <p> Lorem ipusum i forgor the rest </p>
+    <ul>
+      {" "}
+      Here a cool list
+      <li> Pipipi popopo </li>
+      <li> Hello everyone </li>
+      <li> How are you? Fine thank you </li>
+    </ul>
+  </div>
+);
