0745876657da506aaac45e73ec23b63c19e5db1e
PlopesK <popi081004@hotmail.com>
2023-08-30 17:04:43 -0300

    Basic exercise added

--- HTML-CSS-PHP-JS/React/Basic Tests/basic.js
+++ HTML-CSS-PHP-JS/React/Basic Tests/basic.js
@@ -1,27 +1,46 @@
-class MyComponent extends React.Component {
+const JSX = () => {
+  return (
+    <div>
+      <h1> Hello JSX!</h1>
+      <p> Lorem ipusum i forgor the rest </p>
+      <ul>
+        {/* LIST HAHA i don know*/}
+        Here a cool list
+        <li> Pipipi popopo </li>
+        <li> Hello everyone </li>
+        <li> How are you? Fine thank you </li>
+      </ul>
+      <div className="myDiv">
+        <h1>Add a class to this div</h1>
+      </div>
+      <div>
+        <h2>Welcome to React!</h2> <br />
+        <p>Be sure to close all tags!</p>
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+const ChildComponent = () => {
+  return (
+    <div>
+      <p>I am the child</p>
+    </div>
+  );
+};
+
+class ParentComponent extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     return (
       <div>
-        <h1> Hello JSX!</h1>
-        <p> Lorem ipusum i forgor the rest </p>
-        <ul>
-          {/* LIST HAHA i don know*/}
-          Here a cool list
-          <li> Pipipi popopo </li>
-          <li> Hello everyone </li>
-          <li> How are you? Fine thank you </li>
-        </ul>
-        <div className="myDiv">
-          <h1>Add a class to this div</h1>
-        </div>
-        <div>
-          <h2>Welcome to React!</h2> <br />
-          <p>Be sure to close all tags!</p>
-          <hr />
-        </div>
+        <h1>I am the parent</h1>
+        <ChildComponent />
+        <JSX />
       </div>
     );
   }
---
+++ HTML-CSS-PHP-JS/React/Basic Tests/fruits.js
@@ -0,0 +1,52 @@
+const TypesOfFruit = () => {
+  return (
+    <div>
+      <h2>Fruits:</h2>
+      <ul>
+        <li>Apples</li>
+        <li>Blueberries</li>
+        <li>Strawberries</li>
+        <li>Bananas</li>
+      </ul>
+    </div>
+  );
+};
+
+const Fruits = () => {
+  return (
+    <div>
+      <TypesOfFruit />
+    </div>
+  );
+};
+
+const Vegetables = () => {
+  return (
+    <div>
+      <h2>Vegetables: </h2>
+      <ul>
+        <li>Brussel Sprouts</li>
+        <li>Broccoli</li>
+        <li>Squash</li>
+      </ul>
+    </div>
+  );
+};
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        <Fruits />
+        <Vegetables />
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("challenge-node");
+ReactDOM.render(<TypesOfFood />, rootElement);
