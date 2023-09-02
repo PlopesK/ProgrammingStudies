86efd0004845ed13ec23ba9daa881fd6e460f96f
PlopesK <popi081004@hotmail.com>
2023-09-02 17:04:12 -0300

    Testing input with submit

--- HTML-CSS-PHP-JS/React/BasicTests/basic.js
+++ HTML-CSS-PHP-JS/React/BasicTests/basic.js
@@ -106,27 +106,39 @@ class MyComponent extends React.Component {
   }
 }
 
-class ControlledInput extends React.Component {
+class MyForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       input: "",
+      submit: "",
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-
   handleChange(event) {
     this.setState({
       input: event.target.value,
     });
   }
-
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      submit: this.state.input,
+    });
+  }
   render() {
     return (
       <div>
-        <input value={this.state.input} onChange={this.handleChange} />
-        <h4>Controlled Input:</h4>
-        <p>{this.state.input}</p>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.input}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Submit!</button>
+        </form>
+        <h1>{this.state.submit}</h1>
       </div>
     );
   }
