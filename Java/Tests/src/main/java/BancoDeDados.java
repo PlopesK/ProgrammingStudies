5fa2c21fccf1e9e4886d3103adaceb2b9f7595e9
PlopesK <popi081004@hotmail.com>
2023-09-20 16:46:52 -0300

    Testing Before and After

---
+++ Java/Tests/src/main/java/BancoDeDados.java
@@ -0,0 +1,21 @@
+import java.util.logging.Logger;
+
+public class BancoDeDados {
+    private static final Logger LOGGER = Logger.getLogger(BancoDeDados.class.getName());
+
+    public static void iniciarConexao() {
+        LOGGER.info("Iniciou conexão");
+    }
+
+    public static void finalizarConexao() {
+        LOGGER.info("Finalizou conexão");
+    }
+
+    public static void insereDados(Pessoa pessoa) {
+        LOGGER.info("Inseriu dados");
+    }
+
+    public static void removeDados(Pessoa pessoa) {
+        LOGGER.info("Removeu dados");
+    }
+}
---
+++ Java/Tests/src/test/java/ConsultarDadosDePessoaTeste.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.*;
+
+import java.time.LocalDateTime;
+
+public class ConsultarDadosDePessoaTeste {
+    @BeforeAll
+    static void configurarConexao() {
+        BancoDeDados.iniciarConexao();
+    }
+
+    @BeforeEach
+    void insereDadosParaTeste() {
+        BancoDeDados.insereDados(new Pessoa("Carlos", LocalDateTime.of(2000, 2, 1, 13, 0, 0)));
+    }
+
+    @Test
+    void validarDadosDeRetorno() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void validarDadosDeRetorno2() {
+        Assertions.assertNull(null);
+    }
+
+    @AfterEach
+    void removeDadosTeste() {
+        BancoDeDados.removeDados(new Pessoa("Carlos", LocalDateTime.of(2000, 2, 1, 13, 0, 0)));;
+    }
+
+    @AfterAll
+    static void finalizarConexao() {
+        BancoDeDados.finalizarConexao();
+    }
+}
