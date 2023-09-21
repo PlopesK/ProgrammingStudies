47a29343e1f91c67f89066c3e4ae1f6657636232
PlopesK <popi081004@hotmail.com>
2023-09-21 15:29:29 -0300

    Starting Spring Boot project!

---
+++ Java/SpringBoot/FirstProject/src/test/java/plopes/springboot/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package plopes.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
