e2cb5e87e4763e3363e56b38c0265d56ab9a31c4
PlopesK <gp081009@gmail.com>
2024-11-18 21:52:36 -0300

    Finishing all challenges

--- HTML-CSS-PHP-JS/Sites/SitesJS/logica-js/JSGame/JS Game_files/app.js
+++ HTML-CSS-PHP-JS/Sites/SitesJS/logica-js/JSGame/JS Game_files/app.js
@@ -1,22 +1,23 @@
-// let numeroSecreto = parseInt(Math.random() * 11)
-// let tentativas = 1
-// let chute
+alert('Boas vindas ao jogo do número secreto');
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-// alert('Boas vindas ao jogo do número secreto')
+while (chute != numeroSecreto) {
+    chute = prompt("Escolha um número entre 1 e ${numeroMaximo}");
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert("O número secreto é menor que ${chute}");
+        } else {
+            alert("O número secreto é maior que ${chute}");
+        }
+        tentativas++;
+    }
+}
 
-// while (chute != numeroSecreto) {
-//     let chute = prompt('Escolha um número entre 1 e 10')
-//     if (chute == numeroSecreto) {
-//         break
-//     } else {
-//         if (chute > numeroSecreto) {
-//             alert('O número secreto é menor')
-//         } else {
-//             alert('O número secreto é maior')
-//         }
-//     }
-//     tentativas++
-// }
-
-// let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa' 
-// alert(`O número secreto era ${numeroSecreto} e você acertou com apenas ${tentativas} ${palavraTentativa}`)
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert("Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.");
