fb3d2568d29c33077d45b55b43d3d226c8b88fa5
PlopesK <gp081009@gmail.com>
2024-12-02 01:01:28 -0300

    Renaming files using python system

---
+++ Python/rename.py
@@ -0,0 +1,15 @@
+import os
+
+# Programa para renomear arquivos com palavra iguais
+caminho_pasta = r"caminho\para\pasta"
+
+antigo_nome = "antigo"
+novo_nome = "novo"
+
+for nome_arquivo in os.listdir(caminho_pasta):
+    if antigo_nome in nome_arquivo:  # Verifica se o nome antigo está presente
+        novo_nome_arquivo = nome_arquivo.replace(antigo_nome, novo_nome)  # Renomeia o arquivo
+        caminho_antigo = os.path.join(caminho_pasta, nome_arquivo)
+        caminho_novo = os.path.join(caminho_pasta, novo_nome_arquivo)
+        os.rename(caminho_antigo, caminho_novo)  # Renomeia
+        print(f'Renomeado: {nome_arquivo} -> {novo_nome_arquivo}')
